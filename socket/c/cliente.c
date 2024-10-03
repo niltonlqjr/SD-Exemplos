cb457e624d1d5d36369d7a58bde588788701e398
niltonlqjr <niltonlqjr@gmail.com>
2024-10-02 21:18:24 -0300

    c server

---
+++ socket/c/cliente.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Modo de usar\n");
+        printf("%s <mensagem>",argv[0]);
+        return 1;
+    }
+    char host[]="127.0.0.1";
+    char *resp;
+    unsigned int port=5050;
+    struct sockaddr_in addr;
+    int sockfd, len;
+
+    addr.sin_family = AF_INET; 
+    // familia do socket (AF_INET -> IPv4)
+    addr.sin_port = htons(port); 
+    // porta do socket (convertida para o formato correto da porta)
+    addr.sin_addr.s_addr = inet_addr(host);
+    //converte o endereco do server para o formato adequado
+    
+    sockfd = socket(AF_INET,  SOCK_STREAM, 0);
+
+    connect(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr));
+    len = strlen(argv[1]);
+    send(sockfd, argv[1], len, 0);
+    resp = (char *) malloc(len * sizeof(char));
+    recv(sockfd, resp, len, 0);
+    printf("Recebido do servidor: %s\n",resp);
+
+}
