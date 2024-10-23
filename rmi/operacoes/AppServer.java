1737254c1ca0d7fa98550bd928a75fbb2ec16a0f
niltonlqjr <niltonlqjr@gmail.com>
2024-10-23 18:03:24 -0300

    fix server

--- rmi/operacoes/AppServer.java
+++ rmi/operacoes/AppServer.java
@@ -11,16 +11,17 @@ public class AppServer{
     public static void main(String argv[]){
         Integer porta;
         String hostname;
-        if(argv.length < 2){
-            porta = 1099;
-        }else{
-            porta = Integer.valueOf(argv[0]);
-        }
         if(argv.length < 1){
             hostname = "127.0.0.1";
         }else{
-            hostname=argv[1];
+            hostname=argv[0];
         }
+        if(argv.length < 2){
+            porta = 1099;
+        }else{
+            porta = Integer.valueOf(argv[1]);
+        }
+
         String objName = "Operacoes:"+porta.toString();
         OperacoesRemote op = new ServerOperacoes();
 	    
