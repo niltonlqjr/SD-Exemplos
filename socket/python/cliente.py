b1e64e10b427d9ca357b0ea4bef56d9f980e4b47
niltonlqjr <niltonlqjr@gmail.com>
2025-09-02 20:45:50 -0300

    python changes

--- socket/python/cliente.py
+++ socket/python/cliente.py
@@ -1,17 +1,22 @@
 import socket
 import sys
 
-host='10.30.11.43' #localhost
+host='localhost'
 port=5050
 
 if len(sys.argv) <= 1:
     print("forma de usar:")
-    print(sys.argv[0], "<mensagem>")
+    print(sys.argv[0], "<mensagem> [server]")
     exit(1)
 arg = sys.argv[1]
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-s.sendall(bytes(sys.argv[1], 'utf-8'))
-data = s.recv(1024)
-print('Mensagem recebida:', str(data, 'utf-8'))
+if len(sys.argv) > 2:
+    host = sys.argv[2]
+print(f'connectando com o servidor: {host}')
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(bytes(sys.argv[1], 'utf-8'))
+    data = s.recv(1024)
+    print('Mensagem recebida:', str(data, 'utf-8'))
+except:
+    print('erro ao conectar com o servidor')
